eb328459113822ac5b62c8231bbcdf6328f993e1
Sourav992v <sourav992v@gmail.com>
2020-12-06 18:59:58 +0530

    Add paging data adapter

--- app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.codinginflow.imagesearchapp.R
+import com.codinginflow.imagesearchapp.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -12,11 +13,34 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery){
 
     private val viewModel by viewModels<GalleryViewModel>()
 
+    private var _binding : FragmentGalleryBinding? = null
+
+    private val binding get() = _binding!!
+
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        _binding = FragmentGalleryBinding.bind(view)
+
+        var adapter = UnsplashPhotoAdapter()
+
+        binding.apply {
+            recyclerView.setHasFixedSize(true)
+            recyclerView.adapter = adapter
+        }
+
         viewModel.photo.observe(viewLifecycleOwner){
-            
+            adapter.submitData(viewLifecycleOwner.lifecycle, it)
         }
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+
 }
